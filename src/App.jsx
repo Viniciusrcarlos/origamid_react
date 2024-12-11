71da979ed152ec2732be3240965797ae158303dd
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2024-12-10 21:06:23 -0300

    desafio

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,50 @@ import React from 'react';
 
 function App() {
 
+  const luana = {
+    cliente: 'Luana',
+    idade: 27,
+    compras: [
+      { nome: 'Notebook', preco: 'R$ 2500' },
+      { nome: 'Geladeira', preco: 'R$ 3000' },
+      { nome: 'Smartphone', preco: 'R$ 1500' },
+    ],
+    ativa: true,
+  };
+  
+  const mario = {
+    cliente: 'Mario',
+    idade: 31,
+    compras: [
+      { nome: 'Notebook', preco: 'R$ 2500' },
+      { nome: 'Geladeira', preco: 'R$ 3000' },
+      { nome: 'Smartphone', preco: 'R$ 1500' },
+      { nome: 'Guitarra', preco: 'R$ 3500' },
+    ],
+    ativa: false,
+  };
+
+  
+  const dados = mario;
+
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', '')))
+    .reduce((a, b) => a + b);
+
   return (
     <div>
-      App React
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+          {dados.ativa ? 'Ativa' : 'Inativa'}
+        </span>
+      </p>
+      <p>Total: R$ {total}</p>
+      {total > 10000 && <p>Você está gastando muito</p>}
     </div>
-  )
+  );
 }
 
 export default App
