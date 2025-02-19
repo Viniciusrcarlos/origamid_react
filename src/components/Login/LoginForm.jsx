5311d7ad193d6bf54456b4f62c78313ab6229003
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2025-02-18 21:08:57 -0300

    login - corrigindo bug

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
+  const username = useForm('email');
+  const password = useForm();
 
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,7 +16,7 @@ const LoginForm = () => {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({username, password}),
+      body: JSON.stringify(),
     }).then(response => {
       console.log(response); 
       return response.json();
@@ -30,8 +31,8 @@ const LoginForm = () => {
       <h1>Login</h1>
 
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username"/>
-        <Input label="Senha" type="password" name="password"/>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password}/>
         <Button>Entrar</Button>
       </form>
 
