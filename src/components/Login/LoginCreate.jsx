c55477e16972a77a9c79e2f2a9b98544a51853ca
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2025-02-25 21:09:06 -0300

    cadastro

--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginCreate = () => {
+
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  async function handleSubmit (event) {
+    event.preventDefault();
+    const response = await fetch();
+  }
+
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username}/>
+        <Input label="Email" type="email" name="email" {...email}/>
+        <Input label="Senha" type="text" name="password" {...password}/>
+        <Button>Cadastrar</Button>
+      </form>
     </section>
   )
 }
