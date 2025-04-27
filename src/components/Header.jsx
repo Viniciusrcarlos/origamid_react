d76c197b7e95941d3c57504984db1e00222f7b1a
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2025-04-27 20:48:43 -0300

    atualizacoes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,30 @@
-import React from 'react';
-import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
-// import { ReactComponent as Logo } from "../Assets/dogs.svg";
-import MyIcon from "../Assets/dogs.svg";
-import UserContext from '../UserContext';
-
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import MyIcon from "../Assets/dogs.svg?react";
+import UserContext from "../UserContext";
 
 const Header = () => {
-
-  const {data, userLogout} = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
-        <nav className={`${styles.nav} container`}>
-
-          <Link className={styles.logo}  to="/" aria-label='Dogs - Home'> 
-            <img src={MyIcon} alt="Dogs Icon" />
-          </Link>
-
-          {data ? (<Link className={styles.login} to="/conta">
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <MyIcon/>
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
             {data.nome}
-            <button onClick={userLogout}>Sair</button>
-          </Link>) : <Link className={styles.login} to="/login">
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
             Login / Criar
-          </Link>}
-          
-          
-
-        </nav>
+          </Link>
+        )}
+      </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
