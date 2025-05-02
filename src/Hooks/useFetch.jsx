85fca8e1398a050da1a065c0255ec49f4be9e2e8
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2025-05-01 22:15:15 -0300

    modals

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
 
 const useFetch = () => {
-    const [data, setData] = React.useState(null);
-    const [error, setError] = React.useState(null);
-    const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
 
-    const request = React.useCallback(async (URL, options) => {
-        let response;
-        let json;
-        try {
-            setError(null);
-            setLoading(true);
-            response = await fetch(URL, options);
-            json = response.json();
-            if (response.ok === false) {
-                throw new Error(json.message);
-            }
-        } catch (err){
-            json = null;
-            setError(err.message);
-        } finally {
-            setData(json);
-            setLoading(false);
-            return {response, json}
-        }
-    }, [])
-
-    return {
-        data, loading, error, request
+  const request = React.useCallback(async (URL, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(URL, options);
+      json = await response.json();
+      if (response.ok === false) {
+        throw new Error(json.message);
+      }
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
     }
+  }, []);
+
+  return { data, loading, error, request };
 };
 
 export default useFetch;
--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import FeedModal from './FeedModal.jsx'
+import FeedPhotos from './FeedPhotos.jsx'
 
 export const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+
   return (
     <div>
-
+      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto}/>}
+      <FeedPhotos setModalPhoto={setModalPhoto}/>
     </div>
   )
 }
