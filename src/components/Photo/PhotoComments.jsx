73346f9a523fc249d6554588d71069ab41cc01b1
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2025-05-06 21:21:48 -0300

    alteracoes

--- src/components/Photo/PhotoComments.jsx
+++ src/components/Photo/PhotoComments.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { UserContext } from "../../UserContext.jsx";
 import PhotoCommentsForm from "./PhotoCommentsForm";
+import Styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
   const [comments, getComments] = React.useState(() => props.comments);
@@ -8,9 +9,12 @@ const PhotoComments = (props) => {
 
   return (
     <div>
-      <ul>
+      <ul className={styles.comments}>
         {comments.map((comment) => (
-          <li key={comment.comment_ID}></li>
+          <li key={comment.comment_ID}>
+              <b>{comment.comment_author}:  </b>
+              <span>{comment.comment_content}</span>
+          </li>
         ))}
       </ul>
       {login && <PhotoCommentsForm id={props.id} />}
